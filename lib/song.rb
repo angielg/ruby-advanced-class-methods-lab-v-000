6c3d979c89645cc0cfaf02da0d6082f28c2dcba2
Angie Goza <angielg9888@gmail.com>
2019-02-07 23:29:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     song = self.new
     song_info = filename.split(" - ")
     @artist_name = song_info[0]
-    @name = info[1].split(".mp3")
+    @name = song_info[1].split(".mp3")
     song.artist_name = @artist_name
     song.name = @name
     song
